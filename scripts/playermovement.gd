ae747463daada0f039b17088cb791bf817966370
devcshort <13677134+devcshort@users.noreply.github.com>
2025-07-07 17:03:25 -0700

    feat: add block create/destruction

--- scripts/playermovement.gd
+++ scripts/playermovement.gd
@@ -1,20 +1,66 @@
 extends CharacterBody3D
 
+const Util = preload("res://common/util.gd")
+
+const COLLISION_LAYER_AVATAR = 2
+
 var freeze_movement: bool = true
 
 @export var move_speed := 5.0
 @export var jump_velocity := 6.0
 @export var gravity := 20.0
 @export var mouse_sensitivity := 0.002
+@export var terrain_path : NodePath
+@export var cursor_material : Material
 
 @onready var camera_pivot = $CameraPivot
+var _head = null
+
+var _terrain : VoxelTerrain = null
+var _terrain_tool = null
+var _cursor = null
+var _action_place := false
+var _action_remove := false
 
 var pitch := 0.0
 
 func _ready():
+	if terrain_path == NodePath():
+		_terrain = get_parent().get_node(get_parent().terrain)
+		terrain_path = _terrain.get_path() # For correctness
+	else:
+		_terrain = get_node(terrain_path)
+		
+	_head = camera_pivot
+	
+	var mesh = Util.create_wirecube_mesh(Color(0,0,0))
+	var mesh_instance = MeshInstance3D.new()
+	mesh_instance.mesh = mesh
+	if cursor_material != null:
+		mesh_instance.material_override = cursor_material
+	mesh_instance.set_scale(Vector3(1,1,1)*1.01)
+	_cursor = mesh_instance
+	
+	_terrain.add_child(_cursor)
+	_terrain_tool = _terrain.get_voxel_tool()
+		
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func get_pointed_voxel() -> VoxelRaycastResult:
+	var origin = _head.get_global_transform().origin
+	var forward = -_head.get_global_transform().basis.z.normalized()
+	var hit = _terrain_tool.raycast(origin, forward, 10)
+	return hit
+
 func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			match event.button_index:
+				MOUSE_BUTTON_LEFT:
+					_action_remove = true
+				MOUSE_BUTTON_RIGHT:
+					_action_place = true
+					
 	if event is InputEventMouseMotion:
 		# Yaw (horizontal)
 		rotate_y(-event.relative.x * mouse_sensitivity)
@@ -28,6 +74,28 @@ func _physics_process(delta: float):
 		velocity = Vector3.ZERO
 		move_and_slide()
 		return
+		
+	if _terrain == null:
+		return
+	
+	var hit := get_pointed_voxel()
+	if hit != null:
+		_cursor.show()
+		_cursor.set_position(hit.position)
+	else:
+		_cursor.hide()
+	
+	if hit != null:
+		if _action_place:
+			var pos = hit.previous_position
+			if can_place_voxel_at(pos):
+				place(pos)
+		
+		elif _action_remove:
+			dig(hit.position)
+
+	_action_place = false
+	_action_remove = false
 	
 	var input_dir = Vector3.ZERO
 
@@ -58,3 +126,28 @@ func _physics_process(delta: float):
 			velocity.y = jump_velocity
 
 	move_and_slide()
+
+
+func can_place_voxel_at(pos: Vector3i):
+	var space_state = get_viewport().get_world_3d().get_direct_space_state()
+	var params = PhysicsShapeQueryParameters3D.new()
+	params.collision_mask = COLLISION_LAYER_AVATAR
+	params.transform = Transform3D(Basis(), Vector3(pos + Vector3i(1,1,1)) * 0.5)
+	var shape = BoxShape3D.new()
+	var ex = 0.5
+	shape.extents = Vector3(ex, ex, ex)
+	params.set_shape(shape)
+	var hits = space_state.intersect_shape(params)
+	return hits.size() == 0
+
+
+func place(center: Vector3i):
+	_terrain_tool.channel = VoxelBuffer.CHANNEL_TYPE
+	_terrain_tool.value = 1
+	_terrain_tool.do_point(center)
+
+
+func dig(center: Vector3i):
+	_terrain_tool.channel = VoxelBuffer.CHANNEL_TYPE
+	_terrain_tool.value = 0
+	_terrain_tool.do_point(center)
